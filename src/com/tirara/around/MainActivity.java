d77a62b08532183553141931674d65aaaaeebaa5
Acsa Lu <acsa0210@gmail.com>
2014-04-26 18:12:33 +0800

    Create project & show map.

---
+++ src/com/tirara/around/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tirara.around;
+
+import com.google.android.gms.maps.MapFragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
